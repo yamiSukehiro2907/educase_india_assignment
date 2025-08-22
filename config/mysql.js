0f456db3883a7556587436407194ed114c949eca
Vimal290704 <vimalyadavkr001@gmail.com>
2025-08-22 19:49:27 +0530

    prog: added mysql port

--- config/mysql.js
+++ config/mysql.js
@@ -7,6 +7,7 @@ const pool = mysql.createPool({
   user: process.env.MYSQL_USER,
   password: process.env.MYSQL_PASSWORD,
   database: process.env.MYSQL_DATABASE,
+  port: process.env.MYSQL_PORT || 3306,
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
